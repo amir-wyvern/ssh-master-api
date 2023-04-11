4de2434d77acf1fbf16ef3cad17ab95ddc8786e9
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-04-11 14:27:15 +0330

    fix: add deposit router

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from router import ssh, server, user
+from router import ssh, server, user, deposit
 from db import models
 from db.database import engine
 from dotenv import load_dotenv
@@ -11,4 +11,6 @@ app = FastAPI()
 app.include_router(ssh.router)
 app.include_router(user.router)
 app.include_router(server.router)
+app.include_router(deposit.router)
+
 models.Base.metadata.create_all(engine)
