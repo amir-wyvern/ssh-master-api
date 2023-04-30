d0f502c2be86f3470049cacc87feaf300312b621
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-04-30 14:17:16 +0330

    feat: add set_balance method

--- financial_api/user.py
+++ financial_api/user.py
@@ -1,5 +1,5 @@
+from requests.exceptions import ConnectTimeout, ConnectionError, ReadTimeout
 import requests
-from requests.exceptions import ConnectTimeout, ConnectionError
 import os
 
 def header():
@@ -21,11 +21,15 @@ def create_user_if_not_exist(user_id):
 
     except ConnectTimeout:
 
-        return 1419, None
+        return 2419, None
     
     except ConnectionError:
 
-        return 1419, None
+        return 2419, None
+
+    except ReadTimeout:
+
+        return 2419, None
 
 
 def get_balance(user_id):
@@ -36,9 +40,35 @@ def get_balance(user_id):
     
     except ConnectTimeout:
 
-        return 1419, None
+        return 2419, None
 
     except ConnectionError:
 
-        return 1419, None
+        return 2419, None
 
+    except ReadTimeout:
+
+        return 2419, None
+
+
+def set_balance(user_id, new_balance):
+    
+    try:
+        data = {
+            'user_id': user_id,
+            'balance': new_balance
+        }
+        resp = requests.post('http://localhost:8050/user/balance', json=data, headers=header(), timeout=10)
+        return resp.status_code, resp
+    
+    except ConnectTimeout:
+
+        return 2419, None
+
+    except ConnectionError:
+
+        return 2419, None
+
+    except ReadTimeout:
+
+        return 2419, None
