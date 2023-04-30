6da69df14f0f54e7b980884b46589976442e2c89
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-04-30 14:13:45 +0330

    feat: add DbAdmin and DbAgent

--- db/models.py
+++ db/models.py
@@ -4,8 +4,6 @@ from sqlalchemy import (
     Integer,
     String,
     ForeignKey,
-    VARCHAR,
-    Float,
     DateTime,
     Enum
     )
@@ -19,7 +17,8 @@ class DbUser(Base):
     __tablename__ = 'user'
 
     user_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
-    tel_id = Column(Integer, unique=True, index=True, nullable=True)
+    tel_id = Column(String(50), unique=True, index=True, nullable=True)
+    name = Column(String(50), index=True, nullable=True)
     phone_number = Column(Integer, index=True, unique=True, nullable=True)
     email = Column(String(100), index=True, unique=True, nullable=True)
     role = Column(Enum(UserRole), index=True, nullable=False)
@@ -45,6 +44,7 @@ class DbV2rayInterface(Base):
     __tablename__ = 'v2ray_interface'
 
     interface_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
+
 
 class DbV2rayService(Base):
 
@@ -81,6 +81,7 @@ class DbSshService(Base):
     service_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
     server_ip = Column(String(20), ForeignKey('server.server_ip'), index= True, nullable=False)
     user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
+    agent_id = Column(Integer, ForeignKey('user.user_id') , index=True, nullable=True)
     interface_id = Column(Integer, ForeignKey('ssh_interface.interface_id'), index=True, nullable=False)
     username = Column(String(30), index=True, unique= True, nullable=False)
     password = Column(String(30), nullable=False)
@@ -89,3 +90,21 @@ class DbSshService(Base):
     status = Column(Enum(Status), index=True, nullable=False)
     expire = Column(DateTime, index=True, nullable=False)
 
+
+class DbAgent(Base):
+
+    __tablename__ = 'agent'
+    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, primary_key=True)
+    bot_token = Column(String(100), index=True, nullable=True, unique= True)
+    username = Column(String(100), index=True, nullable=False, unique= True)
+    password = Column(String(100), nullable=False)
+    status = Column(Enum(Status), index=True, nullable=False)
+
+
+class DbAdmin(Base):
+
+    __tablename__ = 'admin'
+    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, primary_key=True)
+    bot_token = Column(String(100), index=True, nullable=True, unique= True)
+    username = Column(String(100), index=True, nullable=False, unique= True)
+    password = Column(String(100), nullable=False)
