590a59bad2e3a836b8930ff785e0d2a0a56ddbc3
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-04-30 14:10:30 +0330

    feat: add UserRole and update_role method

--- db/db_user.py
+++ db/db_user.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm.session import Session
 from sqlalchemy import or_
-from schemas import UserRegisterForDataBase
+from schemas import UserRegisterForDataBase, UserRole
 from db.models import DbUser
 from datetime import datetime
 
@@ -10,9 +10,10 @@ def create_user(request: UserRegisterForDataBase, db: Session):
 
     user = DbUser(
         tel_id= request.tel_id,
+        name= request.name,
         phone_number= request.phone_number,
         email= request.email,
-        role= request.role
+        role= request.role,
     )
     db.add(user)
 
@@ -38,6 +39,14 @@ def get_user_by_phone_number(phone_number, db:Session):
 def get_user_by_email(email, db:Session):
     return db.query(DbUser).filter(DbUser.email == email).first()
 
+def update_role(user_id, new_role: UserRole, db:Session):
+
+    user = db.query(DbUser).filter(DbUser.user_id == user_id )
+    user.update({DbUser.role: new_role})
+    db.commit()
+    
+    return user 
+   
 def delete_user(user_id, db:Session):
     user = get_user(user_id, db)
     db.delete(user)
