7402a615be9dfe4c0cd82a74aa8498040592ca1d
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-05-06 16:12:00 +0330

    fix: add some methodes for new style Db

--- db/db_user.py
+++ db/db_user.py
@@ -1,18 +1,24 @@
 from sqlalchemy.orm.session import Session
-from sqlalchemy import or_
-from schemas import UserRegisterForDataBase, UserRole
+from sqlalchemy import and_
+from schemas import (
+    UserRegisterForDataBase,
+    UserRole,
+    Status
+)
 from db.models import DbUser
-from datetime import datetime
 
 
 def create_user(request: UserRegisterForDataBase, db: Session):
 
-
     user = DbUser(
-        tel_id= request.tel_id,
+        chat_id= request.chat_id,
         name= request.name,
         phone_number= request.phone_number,
         email= request.email,
+        bot_token= request.bot_token,
+        username= request.username,
+        password= request.password,
+        status= request.status,
         role= request.role,
     )
     db.add(user)
@@ -21,16 +27,32 @@ def create_user(request: UserRegisterForDataBase, db: Session):
     db.refresh(user)
     return user
 
-def get_all_users(db:Session):
-    return db.query(DbUser).all()
+def get_all_users(db:Session, status: Status= None):
+
+    if status == None:
+        return db.query(DbUser).all()
+    
+    else:
+        return db.query(DbUser).filter(DbUser.status == status).all()
+    
+def get_all_users_by_role(role: UserRole, db:Session, status: Status= None):
+
+    if status == None:
+        return db.query(DbUser).filter(DbUser.role == role).all()
+    
+    else:
+        return db.query(DbUser).filter(and_(DbUser.role == role , DbUser.status == status)).all()
 
 
-def get_user(user_id, db:Session):
+def get_user_by_user_id(user_id, db:Session):
     return db.query(DbUser).filter(DbUser.user_id == user_id ).first()
 
+def get_user_by_bot_token(bot_token, db:Session):
+    return db.query(DbUser).filter(DbUser.bot_token == bot_token ).first()
 
-def get_user_by_telegram_id(tel_id, db:Session):
-    return db.query(DbUser).filter(DbUser.tel_id == tel_id ).first()
+
+def get_user_by_chat_id(chat_id, db:Session):
+    return db.query(DbUser).filter(DbUser.chat_id == chat_id ).first()
 
 def get_user_by_phone_number(phone_number, db:Session):
     return db.query(DbUser).filter(DbUser.phone_number == phone_number).first()
@@ -39,16 +61,51 @@ def get_user_by_phone_number(phone_number, db:Session):
 def get_user_by_email(email, db:Session):
     return db.query(DbUser).filter(DbUser.email == email).first()
 
+
+def get_user_by_username(username, db:Session):
+    return db.query(DbUser).filter(DbUser.username == username ).first()
+    
+
 def update_role(user_id, new_role: UserRole, db:Session):
 
     user = db.query(DbUser).filter(DbUser.user_id == user_id )
     user.update({DbUser.role: new_role})
     db.commit()
     
-    return user 
-   
+    return True
+
+
+def update_password(user_id, password: str, db:Session):
+
+    user = db.query(DbUser).filter(DbUser.user_id == user_id )
+    user.update({DbUser.password: password})
+    db.commit()
+    
+    return True 
+
+
+def update_bot_token(user_id, bot_token: str, db:Session):
+
+    user = db.query(DbUser).filter(DbUser.user_id == user_id )
+    user.update({DbUser.bot_token: bot_token})
+    db.commit()
+    
+    return True 
+
+
 def delete_user(user_id, db:Session):
-    user = get_user(user_id, db)
+
+    user = get_user_by_user_id(user_id, db)
     db.delete(user)
     db.commit()
+
     return True
+
+
+def change_status(user_id, new_status, db: Session):
+
+    user = db.query(DbUser).filter(DbUser.user_id == user_id )
+    user.update({DbUser.status: new_status})
+    db.commit()
+    
+    return True    
