f2a6f6b8cffc1f4796f995d7638897690440c4a0
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-05-06 16:13:19 +0330

    fix: delete DbAdmin and DbAgent and edit DbUser

--- db/models.py
+++ db/models.py
@@ -10,17 +10,21 @@ from sqlalchemy import (
 from schemas import (
     UserRole,
     Status
-)
+)   
 
 class DbUser(Base):
 
     __tablename__ = 'user'
 
     user_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
-    tel_id = Column(String(50), unique=True, index=True, nullable=True)
+    chat_id = Column(String(50), unique=True, index=True, nullable=True)
     name = Column(String(50), index=True, nullable=True)
     phone_number = Column(String(20), index=True, nullable=True)
     email = Column(String(100), index=True, nullable=True)
+    bot_token = Column(String(100), index=True, nullable=True, unique= True)
+    username = Column(String(100), index=True, nullable=False, unique= True)
+    password = Column(String(100), nullable=False)
+    status = Column(Enum(Status), index=True, nullable=False)
     role = Column(Enum(UserRole), index=True, nullable=False)
 
 
@@ -39,18 +43,18 @@ class DbServer(Base):
     status = Column(Enum(Status), index=True, nullable=False)
 
 
-class DbV2rayInterface(Base):
+# class DbV2rayInterface(Base):
 
-    __tablename__ = 'v2ray_interface'
+#     __tablename__ = 'v2ray_interface'
 
-    interface_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
+#     interface_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
 
 
-class DbV2rayService(Base):
+# class DbV2rayService(Base):
 
-    __tablename__ = 'v2ray_service'
+#     __tablename__ = 'v2ray_service'
 
-    service_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
+#     service_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
     # server_ip = Column(String(20), ForeignKey('server.server_ip'), index=True, nullable=False)
     # port = Column(Integer, index=True, nullable=False)
     # transmission: Column(VARCHAR(20), index=True, nullable=False)
@@ -79,32 +83,20 @@ class DbSshService(Base):
     __tablename__ = 'ssh_service'
 
     service_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
-    server_ip = Column(String(20), ForeignKey('server.server_ip'), index= True, nullable=False)
-    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
-    agent_id = Column(Integer, ForeignKey('user.user_id') , index=True, nullable=True)
+    agent_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
+    user_chat_id = Column(String(20), index=True, nullable=True)
+    name = Column(String(50), index=True, nullable=True)
+    phone_number = Column(String(20), index=True, nullable=True)
+    email = Column(String(100), index=True, nullable=True)
+
     interface_id = Column(Integer, ForeignKey('ssh_interface.interface_id'), index=True, nullable=False)
+    server_ip = Column(String(20), ForeignKey('server.server_ip'), index= True, nullable=False)
     username = Column(String(30), index=True, unique= True, nullable=False)
     password = Column(String(30), nullable=False)
     port = Column(Integer, index=True, nullable=False) 
     limit = Column(Integer, nullable=False)
-    status = Column(Enum(Status), index=True, nullable=False)
+
+    created = Column(DateTime, index=True, nullable=False)
     expire = Column(DateTime, index=True, nullable=False)
-
-
-class DbAgent(Base):
-
-    __tablename__ = 'agent'
-    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, primary_key=True)
-    bot_token = Column(String(100), index=True, nullable=True, unique= True)
-    username = Column(String(100), index=True, nullable=False, unique= True)
-    password = Column(String(100), nullable=False)
     status = Column(Enum(Status), index=True, nullable=False)
 
-
-class DbAdmin(Base):
-
-    __tablename__ = 'admin'
-    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, primary_key=True)
-    bot_token = Column(String(100), index=True, nullable=True, unique= True)
-    username = Column(String(100), index=True, nullable=False, unique= True)
-    password = Column(String(100), nullable=False)
