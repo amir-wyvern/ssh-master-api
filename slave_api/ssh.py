d8e385c9eb295e249805247b05d1bff8cacca218
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-05-16 18:26:24 +0330

    feat: add new methodes (groups methods like delete_ssh_account_via_group,...)

--- slave_api/ssh.py
+++ slave_api/ssh.py
@@ -12,13 +12,14 @@ def header():
 
 def create_ssh_account(server_ip, username, password):
     
-    data = {
+    data = { 'users': [{
         'username': username,
         'password': password
-    }
+    }]}
     try:
         resp = requests.post(url=f'http://{server_ip}:8090/ssh/create', json= data, headers= header(), timeout=20)
-        # print('----' ,resp, resp.content)
+        print(resp, resp.content)
+
         return resp.status_code, resp
     
     except ConnectTimeout:
@@ -31,11 +32,26 @@ def create_ssh_account(server_ip, username, password):
         return 2419, None
 
 
+def create_ssh_account_via_group(server_ip, users: list):
+    
+    try:
+        resp = requests.post(url=f'http://{server_ip}:8090/ssh/create', json= { 'users': users}, headers= header(), timeout=20)
+        print(resp, resp.content)
+
+        return resp.status_code, resp
+    
+    except ConnectTimeout:
+        return 2419, None
+    
+    except ConnectionError:
+        return 2419, None
+
+    except ReadTimeout:
+        return 2419, None
+
 def delete_ssh_account(server_ip, username):
     
-    data = {
-        'username': username
-    }
+    data = { 'users':[username]}
     try:
         resp = requests.delete(url=f'http://{server_ip}:8090/ssh/delete', json= data, headers= header(), timeout=20)
         return resp.status_code, resp
@@ -52,11 +68,29 @@ def delete_ssh_account(server_ip, username):
 
         return 2419, None
 
+
+def delete_ssh_account_via_group(server_ip, users):
+    
+    try:
+        resp = requests.delete(url=f'http://{server_ip}:8090/ssh/delete', json= {'users': users}, headers= header(), timeout=20)
+        return resp.status_code, resp
+    
+    except ConnectTimeout:
+
+        return 2419, None
+    
+    except ConnectionError:
+
+        return 2419, None
+
+    except ReadTimeout:
+
+        return 2419, None
+
+
 def block_ssh_account(server_ip, username):
     
-    data = {
-        'username': username
-    }
+    data = {'users':[username]}
     try:
         resp = requests.post(url=f'http://{server_ip}:8090/ssh/block', json= data, headers= header(), timeout=20)
         return resp.status_code, resp
@@ -73,14 +107,30 @@ def block_ssh_account(server_ip, username):
 
         return 2419, None
 
+def block_ssh_account_via_groups(server_ip, users):
+    
+    try:
+        resp = requests.post(url=f'http://{server_ip}:8090/ssh/block', json= {'users': users}, headers= header(), timeout=20)
+        return resp.status_code, resp
+
+    except ConnectTimeout:
+
+        return 2419, None
+    
+    except ConnectionError:
+
+        return 2419, None
+
+    except ReadTimeout:
+
+        return 2419, None
 
 def unblock_ssh_account(server_ip, username):
     
-    data = {
-        'username': username
-    }
+    data = {'users':[username]}
     
     try:
+
         resp = requests.post(url=f'http://{server_ip}:8090/ssh/unblock', json= data, headers= header(), timeout=20)
         return resp.status_code, resp
 
@@ -95,3 +145,22 @@ def unblock_ssh_account(server_ip, username):
     except ReadTimeout:
 
         return 2419, None
+
+def unblock_ssh_account_via_groups(server_ip, users):
+    
+    try:
+        resp = requests.post(url=f'http://{server_ip}:8090/ssh/unblock', json= {'users':users}, headers= header(), timeout=20)
+        return resp.status_code, resp
+
+    except ConnectTimeout:
+
+        return 2419, None
+    
+    except ConnectionError:
+
+        return 2419, None
+
+    except ReadTimeout:
+
+        return 2419, None
+
