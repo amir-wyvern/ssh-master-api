bd3db82d28fc06a7859cc40ce8d495aeb0a4d4b8
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-05-25 18:57:35 +0330

    fix: add try, except for handler connection lost database

--- check_expire.py
+++ check_expire.py
@@ -33,100 +33,106 @@ cache_db = get_redis_cache().__next__()
 
 _, notification_worker = create_worker_from(NotificationCeleryTask)
 
-db = get_db().__next__()
 
 timedelta_2 = timedelta(days=2)
 timedelta_1 = timedelta(days=1)
 
 while True:
     
-    enable_services = db_ssh_service.get_all_services(db, status= ServiceStatus.ENABLE)
-    disable_services = db_ssh_service.get_all_services(db, status= ServiceStatus.DISABLE)
+    try:
+        db = get_db().__next__()
 
-    for service in enable_services:
+        enable_services = db_ssh_service.get_all_services(db, status= ServiceStatus.ENABLE)
+        disable_services = db_ssh_service.get_all_services(db, status= ServiceStatus.DISABLE)
 
-        time_now = datetime.now().replace(tzinfo=pytz.utc)
-        service_expire = service.expire.replace(tzinfo=pytz.utc)
+        for service in enable_services:
 
-        if service.status == ServiceStatus.ENABLE and not get_check_label(service.service_id, cache_db) and service_expire - time_now  < timedelta_2:
-            
-            # if user.tel_id is not None:
+            time_now = datetime.now().replace(tzinfo=pytz.utc)
+            service_expire = service.expire.replace(tzinfo=pytz.utc)
 
-            #     agent = db_agent.get_agent_by_user_id(service.agent_id, db)
-
-                # payload = {
-                #     'tel_id': user.tel_id,
-                #     'bot_token': agent.bot_token,
-                #     'message': 'تنها ۲ روز تا پایان سروریس شما باقی مونده\nدر صورت تمایل میتونید سرویس خود را تمدید کنید',
-                #     'inline_keyboard': None,
-                #     'button_keyboard': None,
-                # }
-                # notification_worker.apply_async(args=(payload,))
-
-            set_check_label(service.service_id, cache_db)
-
-        if service.status == ServiceStatus.ENABLE and not get_check_label(service.service_id, cache_db) and service_expire - time_now < timedelta_1:
-
-            # if user.tel_id is not None:
-
-            #     agent = db_agent.get_agent_by_user_id(service.agent_id, db)
-
-                # payload = {
-                #     'tel_id': user.tel_id,
-                #     'bot_token': agent.bot_token,
-                #     'message': 'تنها ۲ روز تا پایان سروریس شما باقی مونده\nدر صورت تمایل میتونید سرویس خود را تمدید کنید',
-                #     'inline_keyboard': None,
-                #     'button_keyboard': None,
-                # }
-                # notification_worker.apply_async(args=(payload,))
+            if service.status == ServiceStatus.ENABLE and not get_check_label(service.service_id, cache_db) and service_expire - time_now  < timedelta_2:
                 
-            set_check_label(service.service_id, cache_db)
+                # if user.tel_id is not None:
 
-        if service.status == ServiceStatus.ENABLE and time_now > service_expire :
-        
-            status_code ,resp = block_ssh_account(service.server_ip, service.username)
+                #     agent = db_agent.get_agent_by_user_id(service.agent_id, db)
 
-            if status_code == 2419 :
-                logging.info(f'account block failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
-                continue
+                    # payload = {
+                    #     'tel_id': user.tel_id,
+                    #     'bot_token': agent.bot_token,
+                    #     'message': 'تنها ۲ روز تا پایان سروریس شما باقی مونده\nدر صورت تمایل میتونید سرویس خود را تمدید کنید',
+                    #     'inline_keyboard': None,
+                    #     'button_keyboard': None,
+                    # }
+                    # notification_worker.apply_async(args=(payload,))
 
-            if status_code != 200:
-                logging.info(f'account block failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
-                continue
+                set_check_label(service.service_id, cache_db)
+
+            if service.status == ServiceStatus.ENABLE and not get_check_label(service.service_id, cache_db) and service_expire - time_now < timedelta_1:
+
+                # if user.tel_id is not None:
+
+                #     agent = db_agent.get_agent_by_user_id(service.agent_id, db)
+
+                    # payload = {
+                    #     'tel_id': user.tel_id,
+                    #     'bot_token': agent.bot_token,
+                    #     'message': 'تنها ۲ روز تا پایان سروریس شما باقی مونده\nدر صورت تمایل میتونید سرویس خود را تمدید کنید',
+                    #     'inline_keyboard': None,
+                    #     'button_keyboard': None,
+                    # }
+                    # notification_worker.apply_async(args=(payload,))
+                    
+                set_check_label(service.service_id, cache_db)
+
+            if service.status == ServiceStatus.ENABLE and time_now > service_expire :
             
-            db_ssh_service.change_status(service.service_id, ServiceStatus.DISABLE, db)
+                status_code ,resp = block_ssh_account(service.server_ip, service.username)
 
-            logging.info(f'[expire] account blocked [server: {service.server_ip} -username: {service.username}]')
-        
-            # if user.tel_id is not None:
+                if status_code == 2419 :
+                    logging.info(f'account block failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
+                    continue
 
-            #     agent = db_agent.get_agent_by_user_id(service.agent_id, db)
+                if status_code != 200:
+                    logging.info(f'account block failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
+                    continue
+                
+                db_ssh_service.change_status(service.service_id, ServiceStatus.DISABLE, db)
 
-                # payload = {
-                #     'tel_id': user.tel_id,
-                #     'bot_token': agent.bot_token,
-                #     'message': 'تنها ۲ روز تا پایان سروریس شما باقی مونده\nدر صورت تمایل میتونید سرویس خود را تمدید کنید',
-                #     'inline_keyboard': None,
-                #     'button_keyboard': None,
-                # }
-                # notification_worker.apply_async(args=(payload,))
-
-    for service in disable_services:
-        if service.status == ServiceStatus.DISABLE and time_now - service_expire > timedelta(days=30):
-
-            status_code ,resp = delete_ssh_account(service.server_ip, service.username)
-
-            if status_code == 2419 :
-                logging.info(f'account delete failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
-                continue
-
-            if status_code != 200:
-                logging.info(f'account delete failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
-                continue
+                logging.info(f'[expire] account blocked [server: {service.server_ip} -username: {service.username}]')
             
-            db_server.decrease_ssh_accounts_number(service.server_ip, db) 
-            db_ssh_service.change_status(service.service_id, ServiceStatus.DELETED, db)
+                # if user.tel_id is not None:
 
-            logging.info(f'[deleted] account deleted [server: {service.server_ip} -username: {service.username}]')
+                #     agent = db_agent.get_agent_by_user_id(service.agent_id, db)
 
+                    # payload = {
+                    #     'tel_id': user.tel_id,
+                    #     'bot_token': agent.bot_token,
+                    #     'message': 'تنها ۲ روز تا پایان سروریس شما باقی مونده\nدر صورت تمایل میتونید سرویس خود را تمدید کنید',
+                    #     'inline_keyboard': None,
+                    #     'button_keyboard': None,
+                    # }
+                    # notification_worker.apply_async(args=(payload,))
+
+        for service in disable_services:
+            if service.status == ServiceStatus.DISABLE and time_now - service_expire > timedelta(days=30):
+
+                status_code ,resp = delete_ssh_account(service.server_ip, service.username)
+
+                if status_code == 2419 :
+                    logging.info(f'account delete failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
+                    continue
+
+                if status_code != 200:
+                    logging.info(f'account delete failed [server: {service.server_ip} -username: {service.username} -status_code: {resp.status_code}, -content: {resp.content}]')
+                    continue
+                
+                db_server.decrease_ssh_accounts_number(service.server_ip, db) 
+                db_ssh_service.change_status(service.service_id, ServiceStatus.DELETED, db)
+
+                logging.info(f'[deleted] account deleted [server: {service.server_ip} -username: {service.username}]')
+    
+    except Exception as e:
+
+        logging.info(f'[exception]  [{e}]')
+    
     sleep(300)
