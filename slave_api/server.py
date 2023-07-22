0adaeb1ec8f0049229a9fe904e67decc00c87f62
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-07-22 16:32:56 +0330

    feat: add init server for initial

--- slave_api/server.py
+++ slave_api/server.py
@@ -36,3 +36,32 @@ def get_users(server_ip):
 
         return None, HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT, detail={'message': 'ReadTimeout', 'internal_code': 2419})
 
+def init_server(server_ip, ssh_port, manager_password):
+
+    try:
+        data = {
+            'ssh_port': ssh_port,
+            'manager_password': manager_password
+        }
+        for _ in range(2):
+            resp = requests.post(url=f'http://{server_ip}:8090/server/init', json= data, headers= header(), timeout=20)
+            if resp.status_code == 200:
+                break
+
+        if resp.status_code != 200:
+            return None, HTTPException(status_code=resp.status_code ,detail= resp.content.decode())
+        
+        return resp.json(), None
+
+    except ConnectTimeout:
+
+        return None, HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT, detail={'message': 'Connection Timeout', 'internal_code': 2419})
+    
+    except ConnectionError:
+
+        return None, HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT, detail={'message': 'ConnectionError', 'internal_code': 2419})
+
+    except ReadTimeout:
+
+        return None, HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT, detail={'message': 'ReadTimeout', 'internal_code': 2419})
+
