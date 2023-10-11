2497b1104855bafa58f1954e5200a8f083347e95
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-10-11 15:14:49 +0330

    feat: new part for buy server via celery

---
+++ celery_server/main.py
@@ -0,0 +1,219 @@
+import sys
+
+sys.path.append('../')
+
+from cache.cache_session import set_server_number, get_server_number
+from celery_tasks.tasks import ReplaceServerCeleryTask, NotificationCeleryTask
+from celery_tasks.utils import create_worker_from
+from celery_server.server_provider import VPS4
+from cache.database import get_redis_cache
+from checkhost_api.main import check_host
+from fastapi import HTTPException
+from dotenv import load_dotenv
+import traceback
+import requests
+import logging
+import os
+
+load_dotenv('.env')
+
+logger = logging.getLogger('celery_server_service.log')
+logger.setLevel(logging.INFO)
+
+# Create a file handler to save logs to a file 
+
+file_handler = logging.FileHandler('celery_server_service.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+provider_4vps = VPS4()
+
+class ReplaceServerCeleryTaskImpl(ReplaceServerCeleryTask):
+
+    def __init__(self):
+
+        ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
+        ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
+        MANAGER_PASSWORD_SLAVE = os.getenv('MANAGER_PASSWORD_SLAVE')
+        MAIN_URL = os.getenv('MAIN_URL')
+
+        self.main_api = MainApi(MAIN_URL, ADMIN_USERNAME, ADMIN_PASSWORD, MANAGER_PASSWORD_SLAVE)
+
+        if get_server_number(get_redis_cache().__next__()) is None:
+            set_server_number(1, get_redis_cache().__next__())
+
+    def run(self, payload):
+
+        logger.info(f'payload: {payload}')
+
+        try:
+            old_host = payload['host'] 
+            
+            while True:
+
+                while True:
+                    server_ip, server_id, password, location = self.request_new_server()
+                    if server_ip:
+                        break
+                
+                status = check_host(server_ip)
+                if status:
+                    break
+
+                provider_4vps.automatic_renewal(server_id, server_ip)
+            
+            logger.info(f'send requests to submit new server ... (ip: {server_ip})')
+            new_server_resp = self.main_api.submit_new_server(server_ip, password, location)
+            logger.info(f'successfully submited new server (ip: {server_ip} -msg: {new_server_resp})')
+            
+            logger.info(f'send request to transfer server (old_server: {old_host} -new_server: {server_ip})')
+            transfer_server_resp = self.main_api.transfer_server(old_host, server_ip)
+            logger.info(f'successfully transfer new server (old_server: {old_host} -new_server: {server_ip} -msg: {transfer_server_resp})')
+
+        except Exception as e:
+
+            if hasattr(e, 'detail'):
+                error_msg = f'occur an error [err_status: {e.status_code} -err_msg: {e.detail}]'
+                type_alarm = '❗ Error'
+                logger.error(error_msg)
+
+            else:
+                error_msg = f'error [{traceback.print_exc()}]'
+                type_alarm = '🚨 Critical'
+                logger.critical(error_msg)
+            
+            file = 'celery_server/main.py'
+            message = f'{type_alarm}\nFile: {file}\nMessage: {error_msg}'
+
+            payload = {
+                'bot_selector': 'admin_log',
+                'chat_id': 'admin',
+                'message': message
+            }
+            notifocaction_worker.apply_async(args=(payload,))
+
+    def request_new_server(self):
+        
+        current_server_number = int(get_server_number(get_redis_cache().__next__()) )
+        new_server_number = current_server_number + 1
+        new_server_name = f'server-{new_server_number}'
+        set_server_number(new_server_number, get_redis_cache().__next__() )
+
+        server_ip, server_id, password, data_center = provider_4vps.buy_server(new_server_name)
+        if server_ip == False:
+            provider_4vps.automatic_renewal(server_id, server_ip)
+            return False, False, False, False
+        
+        
+        return server_ip, server_id, password, data_center
+
+
+class MainApi:
+
+    def __init__(self, url, admin_username, admin_password, manager_password):
+
+        self.url = url
+        self.manager_password = manager_password
+
+        self.no_token_headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        token = self._login(admin_username, admin_password)
+
+        self.token_headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+
+    def submit_new_server(self, server_ip, password, location):
+        
+        data = {
+            "server_ip": server_ip,
+            "server_type": "main",
+            "location": location,
+            "ssh_port": 6379,
+            "max_users": 30,
+            "ssh_accounts_number": 0,
+            "v2ray_accounts_number": 0,
+            "root_password": password,
+            "manager_password": self.manager_password,
+            "generate_status": "enable",
+            "update_expire_status": "enable",
+            "status": "enable"
+        }
+
+        params = {
+            'deploy_slave': 'enable',
+            'create_domain': 'disable'
+        }
+        
+        resp = requests.post(self.url + 'server/new', params= params, json= data, headers= self.token_headers)
+
+        if resp.status_code != 200:
+            logger.error(f'(submit_new_server) failed to submit new server (ip: {server_ip} -err_status: {resp.status_code} -err_msg: {resp.content})')
+            raise HTTPException(status_code= resp.status_code, detail= resp.json())
+        
+        return resp.json()
+
+
+    def transfer_server(self, old_server_ip, new_server_ip):
+
+        data = {
+            'old_server_ip': old_server_ip,
+            'new_server_ip': new_server_ip,
+            'disable_old_server': True,
+            'delete_old_users': False
+        }
+
+        resp = requests.post(self.url + 'server/transfer', json= data, headers= self.token_headers)
+
+        if resp.status_code != 200:
+            logger.error(f'(transfer_server) failed to transfer server (old_server: {old_server_ip} -new_server: {new_server_ip} -err_status: {resp.status_code} -err_msg: {resp.content})')
+            raise HTTPException(status_code= resp.status_code, detail= resp.json())
+        
+        return resp.json()
+
+
+    def _login(self, admin_username, admin_password):
+
+        data = {
+            'grant_type': '',
+            'username': admin_username,
+            'password': admin_password,
+            'scope': 'admin',
+            'client_id': '',
+            'client_secret': ''
+        }
+
+        resp = requests.post(self.url + 'auth/login', data= data, headers= self.no_token_headers)
+
+        if resp.status_code == 200:
+            return resp.json()['access_token']
+
+        if resp.status_code == 401:
+            return HTTPException(status_code= 6401, detail='Incorrect username or password')
+        
+        if resp.status_code != 200:
+            raise HTTPException(status_code= resp.status_code, detail=resp.content)
+
+
+app, _ = create_worker_from(ReplaceServerCeleryTaskImpl)
+_, notifocaction_worker = create_worker_from(NotificationCeleryTask)
+
+
+# start worker
+if __name__ == '__main__':
+    
+    app.worker_main()
---
+++ celery_server/server_provider.py
@@ -0,0 +1,302 @@
+import sys
+
+sys.path.append('../')
+
+from requests.exceptions import ConnectTimeout, ConnectionError, ReadTimeout
+from fastapi import HTTPException, status
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from random import choice
+from time import sleep
+import requests 
+import logging
+import os
+import re
+
+logger = logging.getLogger('server_provider.log')
+logger.setLevel(logging.INFO)
+
+# Create a file handler to save logs to a file 
+
+file_handler = logging.FileHandler('server_provider.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+load_dotenv('.env')
+
+class VPS4:
+
+    def __init__(self):
+
+        SERVER_PROVIDER_API_KEY_1 = os.getenv('SERVER_PROVIDER_API_KEY_1')
+        SERVER_PROVIDER_URL_1 = os.getenv('SERVER_PROVIDER_URL_1')
+
+        SERVER_PROVIDER_EMAIL_1 = os.getenv('SERVER_PROVIDER_EMAIL_1')
+        SERVER_PROVIDER_PASSWORD_1 = os.getenv('SERVER_PROVIDER_PASSWORD_1')
+
+        if not all([SERVER_PROVIDER_API_KEY_1, SERVER_PROVIDER_URL_1, SERVER_PROVIDER_EMAIL_1, SERVER_PROVIDER_PASSWORD_1]):
+            raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= {'internal_code': 6001, 'detail': 'Cant load env vars for server provider'})
+        
+        self.url = SERVER_PROVIDER_URL_1
+        self.headers = {
+            'accept': 'application/json',
+            'Authorization': f'Bearer {SERVER_PROVIDER_API_KEY_1}'
+        }
+        self.login_data = {
+            'email': SERVER_PROVIDER_EMAIL_1,
+            'password': SERVER_PROVIDER_PASSWORD_1
+        }
+
+        self.dashboard_header = {
+            'headers': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,fa;q=0.8,ar;q=0.7,zh-CN;q=0.6,zh;q=0.5',
+            'Content-Length': '79',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Origin': 'https://4vps.su',
+            'Referer': 'https://4vps.su/dashboard/addserver',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+            'Cookie': 'PHPSESSID=4brorcuijh87lcg92uk0a9js;__ddg1_=5IPFiUxSq4rbzFqYjNUG;googtrans=null;googtrans=null;',
+            'Sec-Ch-Ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': 'Linux',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Sec-Gpc': '1',
+            'Upgrade-Insecure-Requests': '1',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+
+        self.data_center_tmp = { 'Turkiye': 53 ,'Greece': 57, 'Belgium': 62}
+        self.os_list = {'debian11': 25}
+
+    def buy_server(self, server_name):
+        
+        balance = self.get_balance()
+
+        if balance > 1:
+            
+            data_center = choice(list(self.data_center_tmp.keys()))
+            self.login()
+            self._buy_server(server_name, self.data_center_tmp[data_center], self.os_list['debian11'])
+            server_id, server_ip = self.get_ip(server_name)
+
+            self.login()
+            password = self.dashboard_message(server_name, self.data_center_tmp[data_center])
+
+            logger.info('(buyserver) finish buy server and ready for use')
+
+            return server_ip, server_id, password, data_center
+
+        else:
+            raise HTTPException(status_code= 403, detail= {'internal_code': 6002, 'detail': 'not enough balance for new server'})
+            
+    def get_balance(self):
+        
+        resp = requests.get(self.url + 'api/userBalance', headers= self.headers)
+
+        if resp.status_code != 200:
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6002, 'detail': f'error in get user balance (err: {resp.content})'})
+
+        if resp.json()['error'] == True :
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6005, 'detail': f'false result in get user balance (err: {resp.content})'})
+        
+        return resp.json()['data']['userBalance']
+
+    def get_ip(self, server_name):
+
+        creating_flag = False
+        installing_flag = False
+
+        for _ in range(60):
+
+            for server in self.list_servers()[:5]:
+                if server['name'] == server_name:
+                    if server['status'] == 'installing' and not installing_flag:
+                        installing_flag = True
+                        logger.info(f'(get_ip) installing server (name: {server_name})')
+                    
+                    elif server['status'] == 'creating' and not creating_flag:
+                        creating_flag = True
+                        logger.info(f'(get_ip) creating server (name: {server_name})')
+
+                    elif server['status'] == 'active':
+                        logger.info('(get_ip) successfully created server (name: {0} -id: {1} -ip: {2})'.format(server_name, server['id'], server['ipv4']))
+                        return server['id'], server['ipv4']
+
+
+            sleep(10)
+
+        logger.error(f'(get_ip) failed to created server (name: {server_name})')
+        return False, False
+
+    def list_servers(self):
+        
+        resp = requests.get(self.url + 'api/myservers', headers= self.headers)
+
+        if resp.status_code != 200:
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6002, 'detail': f'error in get list servers (err: {resp.content})'})
+
+        if resp.json()['error'] == True :
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6004, 'detail': f'false result in get list servers (err: {resp.content})'})
+        
+        return resp.json()['data']['serverlist']
+
+    def automatic_renewal(self, server_id, server_ip):
+
+        data = {
+            'serverid': server_id
+        }
+        resp = requests.post(self.url + 'api/action/autoprolong', data= data, headers= self.headers)
+
+        if resp.status_code != 200:
+            logger.error(f'(automatic_renewal) failed in renewal status server (id: {server_id} -ip: {server_ip} -err_status: {resp.status_code} -err_msg: {resp.content})')
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6002, 'detail': f'error in post auto renewal (err: {resp.content})'})
+
+        if resp.json()['error'] == True :
+            logger.error(f'(automatic_renewal) failed in renewal status server (id: {server_id} -ip: {server_ip} -err_status: {resp.status_code} -err_msg: {resp.content})')
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6003, 'detail': f'false result in post auto renewal (err: {resp.content})'})
+        
+        renewl_status = resp.json()['data']
+        logger.info(f'(automatic_renewal) renewal status server [{server_ip}] change to [{renewl_status}]  ')
+        
+        return renewl_status
+    
+    def list_data_center(self):
+
+        resp = requests.get(self.url + 'api/getDcList', headers= self.headers)
+
+        if resp.status_code != 200:
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6006, 'detail': f'error in get list data center (err: {resp.content})'})
+
+        if resp.json()['error'] == True :
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6007, 'detail': f'false result in get list data center (err: {resp.content})'})
+        
+        
+        return resp.json()['data']
+    
+    def list_tariff(self):
+
+        resp = requests.get(self.url + 'api/getTarifList', headers= self.headers)
+
+        if resp.status_code != 200:
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6006, 'detail': f'error in get list data center (err: {resp.content})'})
+
+        if resp.json()['error'] == True :
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6007, 'detail': f'false result in get list data center (err: {resp.content})'})
+        
+        return resp.json()['data']
+
+    def list_images(self):
+
+        resp = requests.get(self.url + f'https://4vps.su/api/getImages/13/16', headers= self.headers)
+
+        if resp.status_code != 200:
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6008, 'detail': f'error in get list os images (err: {resp.content})'})
+
+        if resp.json()['error'] == True :
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6009, 'detail': f'false result in get list os images (err: {resp.content})'})
+        
+        return resp.json()['data']['images']
+    
+    def login(self):
+
+        resp = requests.post('https://4vps.su/account/action/login', data= self.login_data, headers= self.dashboard_header)
+
+        if resp.status_code != 200:
+            logger.error(f'(login) failed login (err_status: {resp.status_code} -err_msg: {resp.content}')
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6010, 'detail': f'failed to login to dashboard (err: {resp.content})'})
+
+        if resp.json()['status'] != 'success':
+            logger.error(f'(login) failed login (err_status: {resp.status_code} -err_msg: {resp.content}')
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6010, 'detail': f'false result in login to dashboard (err: {resp.content})'})
+
+        logger.info('(login) successfully login')
+        return True
+    
+    def _buy_server(self, name, data_center, os_image):
+        
+        data = {
+            'tarif': 13,
+            'datacenter': data_center,
+            'ostempl': os_image,
+            'recipe': '',
+            'name': name,
+            'count': 1,
+            'period': 1,
+            'promocode':'' 
+        }
+        resp = requests.post('https://4vps.su/dashboard/action/buyServer4', data= data, headers= self.dashboard_header)
+
+        if resp.status_code != 200:
+            logger.error(f'(_buy server) failed (err_status: {resp.status_code} -err_msg: {resp.content})')
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6010, 'detail': f'error in buy server (err: {resp.content} -datacenter: {data_center})'})
+
+        if resp.json()['status'] != 'success' :
+            logger.error(f'(_buy server) failed (err_status: {resp.status_code} -err_msg: {resp.content})')
+            raise HTTPException(status_code= resp.status_code, detail= {'internal_code': 6003, 'detail': f'false result in buy server (err: {resp.content} -datacenter: {data_center})'})
+        
+        logger.info(f'(buy server) successfuly buy a server (name: {name} -datacenter: {data_center})')
+        
+        return True
+
+    def dashboard_message(self, server_name, data_center):
+
+        data = {
+            'tarif': 13,
+            'datacenter': data_center,
+            'ostempl': 25,
+            'recipe': '',
+            'name': server_name,
+            'count': 1,
+            'period': 1,
+            'promocode':'' 
+        }
+
+        for _ in range(10):
+            
+            try:
+                resp = requests.get('https://4vps.su/dashboard/messages', data= data, headers= self.dashboard_header)
+                if resp.status_code != 200:
+                    logger.error(f'false result in get dashboard message (err: {resp.content})')
+                    continue
+
+                break
+
+            except (ConnectTimeout, ConnectionError, ReadTimeout):
+                logger.error('error in get dashboard message (TimeOut)')
+                sleep(1)
+        
+        return self._scrap(resp.text, server_name)
+        
+    def _scrap(self, text, server_name):
+
+        soup = BeautifulSoup(text, 'html.parser')
+
+        specific_divs = soup.find_all('div', class_='message-block')[:3]
+
+        for div in specific_divs:
+
+            server_title = div.find('div', class_='header__title')
+            match = re.search(f'{server_name}', server_title.text)
+
+            password_obj = div.find('b', class_='eventCopy')
+            if hasattr(password_obj, 'text') and match:
+                server = match.group()
+                password = password_obj.text
+                
+                if f'{server}' == server_name:
+                    return password
+        
+        return False
