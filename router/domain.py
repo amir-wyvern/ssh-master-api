7abbe385c9b2234e8e71d829667dbffd10974674
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-10-13 02:48:02 +0330

    fix: fix bugs

--- router/domain.py
+++ router/domain.py
@@ -255,15 +255,16 @@ def transfer_configs_via_domain(request: DomainTransfer, current_user: TokenUser
             logger.error(f'[transfer domain] (domain) failed to update cloudflare records (domain: {old_domain.domain_name} -new_server: {request.new_server_ip} -err_code: {err.status_code} -err_resp: {err.detail})')
             raise err
 
-        old_server_ip = old_domain.server_ip
-        db_domain.update_server_ip(old_domain.domain_id, request.new_server_ip, db)
         logger.info(f'[transfer domain] (domain) successfully updated domain (domain: {old_domain.domain_name} -from_server_ip: {old_server_ip} -to_server_ip: {request.new_server_ip} )')
+        old_server_ip = old_domain.server_ip
         
         if request.delete_old_users:
-            db_server.decrease_ssh_accounts_number(old_domain.server_ip, db, commit= False, number= len(set(resp_del['success_users']))) 
+            db_server.decrease_ssh_accounts_number(old_server_ip, db, commit= False, number= len(set(resp_del['success_users']))) 
         
         db_server.increase_ssh_accounts_number(request.new_server_ip, db, commit= False, number= len(resp_create['success_users']))
         db.commit()
+        
+        db_domain.update_server_ip(old_domain.domain_id, request.new_server_ip, db)
 
         resp = DomainTransferResponse(
             old_domain_name= old_domain.domain_name,
