cb4d025005636203f36e2e32a7c8c37c77ac6323
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-10-17 10:11:50 +0330

    fix: change in commands in init server

--- router/server.py
+++ router/server.py
@@ -98,10 +98,6 @@ def add_new_server(request: NewServer, deploy_slave: DeployStatus = DeployStatus
             'python -m venv /root/ssh-slave-api/venv',
             'tmux new-session -d -s slave',
             "tmux send-keys -t slave 'cd /root/ssh-slave-api;source venv/bin/activate;pip install -r requirements.txt;uvicorn main:app --host 0.0.0.0 --port 8090' Enter",
-            'wget -O /usr/bin/badvpn-udpgw https://raw.githubusercontent.com/daybreakersx/premscript/master/badvpn-udpgw64',
-            'chmod +x /usr/bin/badvpn-udpgw',
-            "echo -e '#!/bin/sh -e\nscreen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300\nscreen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7301\ntmux new-session -d -s slave\ntmux send-keys -t slave 'cd /root/ssh-slave-api;source venv/bin/activate;pip install -r requirements.txt;uvicorn main:app --host 0.0.0.0 --port 8090' Enter\nexit 0' > /etc/rc.local",
-            'chmod +x /etc/rc.local',
             'shutdown -r -t 2',
         ]
 
