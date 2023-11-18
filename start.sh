8b306bf6028e7a3701ac20bc5fcad4dc3c7d4a82
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-11-18 06:35:00 -0500

    fix: add backup service

--- start.sh
+++ start.sh
@@ -12,7 +12,16 @@ else
 fi
 
 tmux send-keys -t master:0.1 'cd /root/ssh-master-api;source venv/bin/activate;python extensions/check_service_time/check_expire.py' Enter
-tmux send-keys -t master:0.2 'cd /root/ssh-master-api;source venv/bin/activate;celery -A celery_notification.main worker --concurrency=4 --loglevel=info -n notification_worker.%h' Enter
+
+
+# notif
+tmux new-session -d -s notif
+tmux send-keys -t notif 'cd /root/ssh-master-api;source venv/bin/activate;celery -A celery_notification.main worker --concurrency=4 --loglevel=info -n notification_worker.%h' Enter
+
+
+# backup
+tmux send-keys -t master:0.1 'cd /root/ssh-master-api;source venv/bin/activate;python backup_service/main.py' Enter
+
 
 # financial
 tmux new-session -d -s financial
@@ -25,6 +34,7 @@ fi
 tmux send-keys -t financial:0.0 'cd /root/financial-vpn-api;source venv/bin/activate;uvicorn main:app --port 8050' Enter
 tmux send-keys -t financial:0.1 'cd /root/financial-vpn-api;source venv/bin/activate;celery -A celery_transfer.main worker --concurrency=4 --loglevel=info -n transfer_worker.%h' Enter
 
+
 # telegram
 tmux new-session -d -s telegram
 if [ -d "/root/financial-vpn-api/venv" ]; then
@@ -33,16 +43,17 @@ else
     tmux send-keys -t telegram 'cd /root/telegram-client-vpn;python -m venv /root/financial-vpn-api/venv;pip install -r requirements.txt;python main.py' Enter
 fi
 
+
 # slave 
-tmux new-session -d -s slave
-if [ -d "/root/ssh-slave-api/venv" ]; then
-    tmux send-keys -t slave 'cd /root/ssh-slave-api;source venv/bin/activate;uvicorn main:app --host 0.0.0.0 --port 8090' Enter
-else
-    tmux send-keys -t slave 'cd /root/ssh-slave-api;python -m venv /root/ssh-slave-api/venv;source venv/bin/activate;pip install -r requirements.txt;uvicorn main:app --host 0.0.0.0 --port 8090' Enter
-fi
+# tmux new-session -d -s slave
+# if [ -d "/root/ssh-slave-api/venv" ]; then
+#     tmux send-keys -t slave 'cd /root/ssh-slave-api;source venv/bin/activate;uvicorn main:app --host 0.0.0.0 --port 8090' Enter
+# else
+#     tmux send-keys -t slave 'cd /root/ssh-slave-api;python -m venv /root/ssh-slave-api/venv;source venv/bin/activate;pip install -r requirements.txt;uvicorn main:app --host 0.0.0.0 --port 8090' Enter
+# fi
 
 # check-servers
 tmux new-session -d -s check-servers
 tmux split-window -v -t check-servers
-tmux send-keys -t check-servers:0.0 'cd /root/ssh-master-api;source venv/bin/activate;celery -A celery_server.main worker --concurrency=4 --loglevel=info -n server_service_worker.%h' Enter
+# -- tmux send-keys -t check-servers:0.0 'cd /root/ssh-master-api;source venv/bin/activate;celery -A celery_server.main worker --concurrency=4 --loglevel=info -n server_service_worker.%h' Enter
 tmux send-keys -t check-servers:0.1 'cd /root/ssh-master-api;source venv/bin/activate;python extensions/server_check/main.py' Enter
