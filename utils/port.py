8beee97bfceb03dbe1688feeb3e94d23667ae08a
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-11-20 03:08:00 -0500

    build: add new lib for dynamic port

---
+++ utils/port.py
@@ -0,0 +1,9 @@
+available_ports = [11570, 6379, 5984, 27017, 7000, 10580, 1521, 5432, 1433, 22590]
+
+def generate_port(username):
+
+    number = int(username.split('_')[1])
+    
+    return available_ports[number % len(available_ports)]
+
+
