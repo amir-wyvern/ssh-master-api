db7ae24cc311c14d39fc7c9226f69872c2e3887c
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-11-20 12:29:42 -0500

    fix: fix bug

--- utils/port.py
+++ utils/port.py
@@ -1,8 +1,12 @@
-available_ports = [11570, 6379, 5984, 27017, 7000, 10580, 1521, 5432, 1433, 22590]
+available_ports = [11570, 6379, 5984, 27017, 7000, 10580, 1521, 5432] #, 1433, 22590]
 
 def generate_port(username):
 
-    number = int(username.split('_')[1])
+    if len(username.split('_')) == 1:
+        number = 1
+        
+    else:
+        number = int(username.split('_')[1])
     
     return available_ports[number % len(available_ports)]
 
